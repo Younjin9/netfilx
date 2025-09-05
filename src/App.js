106362d94ccc9385c7de80ae7bf7a4ea9da1d16f
You Kyoung <pyk710_@naver.com>
2025-09-05 15:19:09 +0900

    Row 추가

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import { Outlet, Route, Routes } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
+import MainPage from './pages/MainPage';
+
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  )
+}
+
+
+function App() {
+  return  (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+  
+}
+
+export default App;
