12fde177c1231d6b2959cda4555e1194aab8853e
RealND7 <maxicanapark@gmail.com>
2025-09-09 18:28:35 +0900

    Update Banner.js
    YS_250909

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,74 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../api/axios'; // TMDB API 요청을 위한 axios 인스턴스 import
-import requests from '../api/requests'; // API endpoint 모음 import
-
-import './Banner.css' // 배너 스타일 import
-
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
+import requests from "../api/requests";
+import "./Banner.css";
+import styled from "styled-components";
 export default function Banner() {
+  const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
-    // 영화 정보 상태
-    const [movie, setMovie] = useState({});
-    // Play 버튼 클릭 여부 상태
-    const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    // 컴포넌트 마운트 시 영화 데이터 fetch
-    useEffect(() => {
-        fetchData();
-    }, []);    
+  const fetchData = async () => {
+    // 현재 상영중인 영화 정보를 가져오기(여러 영화)
+    const request = await axios.get(requests.fetchNowPlaying);
 
-    // 영화 데이터 불러오는 함수
-    const fetchData = async () => {
-        // 현재 상영 중인 영화 목록 요청
-        const request = await axios.get(requests.fetchNowPlaying);
+    // 여러 영화 중 영화 하나의 ID를 가져오기
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
 
-        // 랜덤으로 영화 하나 선택
-        const movieId =
-            request.data.results[
-                Math.floor(Math.random() * request.data.results.length)
-            ].id;
+    // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+    setMovie(movieDetail);
+  };
 
-        // 선택한 영화의 상세 정보 요청 (비디오 정보 포함)
-        const { data: movieDetail } = await axios.get(
-            `movie/${movieId}`,
-            {
-                params: { append_to_response: "videos" },
-            }
-        );
-        // 상태에 영화 정보 저장
-        setMovie(movieDetail);
-    };
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
 
-    // 긴 텍스트를 n글자까지만 보여주고 ... 처리하는 함수
-    const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-    };
-
-    // 배너 UI 렌더링
+  console.log('movie',movie);
+  if (!isClicked) {
     return (
-        <header
-            className="banner"
-            style={{
-                backgroundImage: movie.backdrop_path
-                    ? `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`
-                    : "none",
-                backgroundPosition: "top center",
-                backgroundSize: "cover"
-            }}>
-            <div className="banner__contents">
-                {/* 영화 제목 */}
-                <h1 className='banner__title'>{movie.title || movie.name || movie.original_name} </h1>
-                <div className="banner__buttons">
-                    {/* Play 버튼 */}
-                    <button className="banner__button play" onClick={() => setIsClicked(true)}>Play</button>
-                    {/* More Information 버튼 */}
-                    <button className="banner__info">
-                        <div className='space'></div>More Information</button>
-                </div>
-                {/* 영화 설명 */}
-                <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-            </div>
-            {/* 아래쪽 그라데이션 효과 */}
-            <div className="banner--fadeBottom" />
-        </header>
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie.overview, 100)}
+          </h1>
+        </div>
+        <div className="banner--fadeBottom" />
+      </header>
     );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
